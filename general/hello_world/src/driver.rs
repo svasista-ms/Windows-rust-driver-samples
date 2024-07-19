32e8c694ea2a58bbbdc9c7e776a8bb1c92556a4f
Shravan Vasista <svasista@microsoft.com>
2024-07-19 00:41:44 -0700

    Add wdf_object_context procedural macros to Hello World Driver

--- general/hello_world/src/driver.rs
+++ general/hello_world/src/driver.rs
@@ -1,20 +1,14 @@
 use wdk::{nt_success, paged_code, println};
 use wdk_sys::{
-    macros,
-    ntddk::KeGetCurrentIrql,
-    APC_LEVEL,
-    DRIVER_OBJECT, GUID, NTSTATUS, PCUNICODE_STRING, PDRIVER_OBJECT, ULONG, WDFDEVICE, WDFDEVICE_INIT, WDFDRIVER, WDF_DRIVER_CONFIG, WDF_NO_HANDLE, WDF_NO_OBJECT_ATTRIBUTES, WDF_OBJECT_ATTRIBUTES, _WDF_PNPPOWER_EVENT_CALLBACKS, 
-    // _WDF_PNPPOWER_EVENT_CALLBACKS
-    // _WDF_OBJECT_ATTRIBUTES,
+    macros, ntddk::KeGetCurrentIrql, APC_LEVEL, DRIVER_OBJECT, NTSTATUS, PCUNICODE_STRING, PDRIVER_OBJECT, PWDFDEVICE_INIT, ULONG, WDFDEVICE, WDFDEVICE_INIT, WDFDRIVER, WDFOBJECT, WDF_DRIVER_CONFIG, WDF_NO_HANDLE, WDF_NO_OBJECT_ATTRIBUTES,
+    WDF_OBJECT_ATTRIBUTES, 
+    WDF_PNPPOWER_EVENT_CALLBACKS, 
+    _WDF_EXECUTION_LEVEL, _WDF_SYNCHRONIZATION_SCOPE
 };
 
+use crate::{wdf_object_context::wdf_get_context_type_info, wdf_object_get_device_context, DeviceContext};
+use crate::{WDF_DEVICE_CONTEXT_TYPE_INFO, WDF_REQUEST_CONTEXT_TYPE_INFO};
 
-const GUID_DEVINTERFACE_HELLO_WORLD: GUID = GUID {
-    Data1: 0x4d36e988,
-    Data2: 0xe315,
-    Data3: 0x11ce,
-    Data4: [0xbf, 0xc1, 0x08, 0x01, 0x2b, 0xe1, 0x03, 0x18],
-};
 
 #[link_section = "INIT"]
 #[export_name = "DriverEntry"] // WDF expects a symbol with the name DriverEntry
@@ -30,15 +24,7 @@ extern "system" fn driver_entry(
     println!("Driver: {:?}", driver);
 
     println!("Registry Path: {:?}", registry_path);
-    
-    let mut _driver_object_attributes = WDF_OBJECT_ATTRIBUTES {
-        Size: core::mem::size_of::<WDF_OBJECT_ATTRIBUTES>() as ULONG,
-        // EvtCleanupCallback: Some(evt_driver_object_cleanup),
-        // EvtDestroyCallback: Some(evt_driver_object_destroy),
-        ..WDF_OBJECT_ATTRIBUTES::default()
-    };
-    
-    // let attributes_ptr_mut: *mut _WDF_OBJECT_ATTRIBUTES = ; 
+
     
     let mut driver_config = WDF_DRIVER_CONFIG {
         Size: core::mem::size_of::<WDF_DRIVER_CONFIG>() as ULONG,
@@ -47,19 +33,25 @@ extern "system" fn driver_entry(
         ..WDF_DRIVER_CONFIG::default()
     };
     
-    let driver_handle: *mut WDFDRIVER = WDF_NO_HANDLE.cast::<WDFDRIVER>();
+    println!("Driver Config: {:?}", driver_config);
+    
+    let driver_handle = WDF_NO_HANDLE.cast::<WDFDRIVER>();
 
     let nt_status = unsafe {
         macros::call_unsafe_wdf_function_binding!(
             WdfDriverCreate,
             driver as PDRIVER_OBJECT,
             registry_path,
+            // (core::ptr::addr_of_mut!(driver_object_attributes)) as *mut WDF_OBJECT_ATTRIBUTES,
+            // &mut driver_object_attributes as *mut WDF_OBJECT_ATTRIBUTES,
+            // &mut driver_object_attributes,
             WDF_NO_OBJECT_ATTRIBUTES,
-            // &mut driver_object_attributes as *mut _WDF_OBJECT_ATTRIBUTES,
             &mut driver_config,
             driver_handle
         )
     };
+
+    println!("NTSTATUS: {:?}, Driver Handle After WdfDriverCreate: {:?}", nt_status, driver_handle);
 
     if !nt_status == 0 {
         println!("Error: WdfDriverCreate failed {nt_status:#010X}");
@@ -72,7 +64,133 @@ extern "system" fn driver_entry(
 }
 
 
+
+// Driver Config
+#[link_section = "PAGE"]
+extern "C" fn evt_device_add(
+    driver: WDFDRIVER, 
+    device_init: PWDFDEVICE_INIT
+) -> NTSTATUS {
+    paged_code!();
+    
+    println!("Enter: evt_device_add");
+    
+    println!("Driver: {:?}", driver);
+
+    println!("Device Init: {:?}", device_init);
+
+    let mut device_init = unsafe {
+        device_init
+        .as_mut()
+        .expect("Device Init cannot be null")
+    };
+
+    println!("Device Init: {:?}", device_init);
+
+    let mut pnp_power_callbacks: WDF_PNPPOWER_EVENT_CALLBACKS = WDF_PNPPOWER_EVENT_CALLBACKS {
+        Size: core::mem::size_of::<WDF_PNPPOWER_EVENT_CALLBACKS>() as ULONG,
+        ..WDF_PNPPOWER_EVENT_CALLBACKS::default()
+    };
+    
+    println!("Pnp Power Callbacks: {:?}", pnp_power_callbacks);
+    
+    let () = unsafe {
+        macros::call_unsafe_wdf_function_binding!(
+            WdfDeviceInitSetPnpPowerEventCallbacks,
+            device_init,
+            &mut pnp_power_callbacks
+        )
+    };
+    
+    println!("Pnp Power Callbacks Set");
+    
+    let mut request_attributes = WDF_OBJECT_ATTRIBUTES {
+        Size: core::mem::size_of::<WDF_OBJECT_ATTRIBUTES>() as ULONG,
+        ExecutionLevel: _WDF_EXECUTION_LEVEL::WdfExecutionLevelInheritFromParent,
+        SynchronizationScope: _WDF_SYNCHRONIZATION_SCOPE::WdfSynchronizationScopeInheritFromParent,
+        ContextTypeInfo: wdf_get_context_type_info!(RequestContext),
+        ..WDF_OBJECT_ATTRIBUTES::default()
+    };
+
+    let () = unsafe {
+        macros::call_unsafe_wdf_function_binding!(
+            WdfDeviceInitSetRequestAttributes,
+            device_init,
+            // WDF_NO_OBJECT_ATTRIBUTES
+            &mut request_attributes
+        )
+    };
+    
+    println!("Request Attributes Set");
+    
+    let mut device_attributes = WDF_OBJECT_ATTRIBUTES {
+        Size: core::mem::size_of::<WDF_OBJECT_ATTRIBUTES>() as ULONG,
+        ExecutionLevel: _WDF_EXECUTION_LEVEL::WdfExecutionLevelInheritFromParent,
+        SynchronizationScope: _WDF_SYNCHRONIZATION_SCOPE::WdfSynchronizationScopeInheritFromParent,
+        ContextTypeInfo: wdf_get_context_type_info!(DeviceContext),
+        ..WDF_OBJECT_ATTRIBUTES::default()
+    };
+
+    println!("Device Attributes: {:?}", device_attributes);
+    
+    let mut device: WDFDEVICE = WDF_NO_HANDLE as WDFDEVICE;
+
+    println!("Device Handle Before WdfDeviceCreate: {:?}", device);
+    
+    let mut nt_status: NTSTATUS = unsafe {
+        macros::call_unsafe_wdf_function_binding!(
+            WdfDeviceCreate,
+            (core::ptr::addr_of_mut!(device_init)) as *mut *mut WDFDEVICE_INIT,
+            // WDF_NO_OBJECT_ATTRIBUTES,
+            &mut device_attributes,
+            &mut device
+        )
+    };
+
+    println!("Device Handle After WdfDeviceCreate: {:?}", device);
+    
+    println!("NTSTATUS: {:?} \n Device: {:?}", nt_status, device);
+    
+    if !nt_success(nt_status) {
+        println!("Error: WdfDeviceCreate failed {nt_status:#010X}");
+        return nt_status;
+    }
+
+    let device_context: *mut DeviceContext =
+        unsafe { wdf_object_get_device_context(device as WDFOBJECT) };
+    unsafe { (*device_context).private_device_data = 0 };
+
+    println!("Device Context: {:?}", device_context);
+
+    nt_status = unsafe {
+        macros::call_unsafe_wdf_function_binding!(
+            WdfDeviceCreateDeviceInterface,
+            device,
+            &crate::GUID_DEVINTERFACE_HELLO_WORLD,
+            core::ptr::null_mut()
+        )
+    };
+    
+    if !nt_success(nt_status) {
+        println!("Error: WdfDeviceCreateDeviceInterface failed {nt_status:#010X}");
+        return nt_status;
+    }
+
+    println!("Exit: evt_device_add");
+    
+    nt_status
+}
+
+extern "C" fn evt_driver_unload(driver: WDFDRIVER) {
+    println!("Enter: evt_driver_unload");
+
+    println!("Driver: {:?}", driver);
+    
+    println!("Exit: evt_driver_unload");
+}
+
 // Driver Object Attributes
+
 // extern "C" fn evt_driver_object_cleanup(driver_object: WDFOBJECT){
 //     println!("Enter: evt_driver_object_cleanup");
 
@@ -88,101 +206,3 @@ extern "system" fn driver_entry(
 
 //     println!("Exit: evt_driver_object_destroy");
 // }
-
-// Driver Config
-#[link_section = "PAGE"]
-extern "C" fn evt_device_add(
-    driver: WDFDRIVER, 
-    device_init: *mut WDFDEVICE_INIT
-) -> NTSTATUS {
-    paged_code!();
-
-    println!("Enter: evt_device_add");
-
-    println!("Driver: {:?}", driver);
-
-    println!("Device Init: {:?}", device_init);
-
-    let mut device_init = unsafe {
-        device_init
-        .as_mut()
-        .expect("Device Init cannot be null")
-    };
-
-    println!("Device Init: {:?}", device_init);
-
-    let mut pnp_power_callbacks: _WDF_PNPPOWER_EVENT_CALLBACKS = _WDF_PNPPOWER_EVENT_CALLBACKS {
-        Size: core::mem::size_of::<_WDF_PNPPOWER_EVENT_CALLBACKS>() as ULONG,
-        .._WDF_PNPPOWER_EVENT_CALLBACKS::default()
-    };
-
-    println!("Pnp Power Callbacks: {:?}", pnp_power_callbacks);
-
-    let () = unsafe {
-        macros::call_unsafe_wdf_function_binding!(
-            WdfDeviceInitSetPnpPowerEventCallbacks,
-            device_init,
-            &mut pnp_power_callbacks
-        )
-    };
-
-    println!("Pnp Power Callbacks Set");
-
-    // let _ = unsafe {
-    //     macros::call_unsafe_wdf_function_binding!(
-    //         WdfDeviceInitSetRequestAttributes,
-    //         device_init,
-    //         WDF_NO_OBJECT_ATTRIBUTES,
-    //     )
-    // };
-
-    // println!("Request Attributes Set");
-
-    let mut device: WDFDEVICE = WDF_NO_HANDLE as WDFDEVICE;
-
-    println!("Device Handle Before WdfDeviceCreate: {:?}", device);
-
-    let mut nt_status: NTSTATUS = unsafe {
-        macros::call_unsafe_wdf_function_binding!(
-            WdfDeviceCreate,
-            (core::ptr::addr_of_mut!(device_init)) as *mut *mut WDFDEVICE_INIT,
-            WDF_NO_OBJECT_ATTRIBUTES,
-            &mut device
-        )
-    };
-
-    println!("Device Handle After WdfDeviceCreate: {:?}", device);
-
-    println!("NTSTATUS: {:?} \n Device: {:?}", nt_status, device);
-
-    if !nt_success(nt_status) {
-        println!("Error: WdfDeviceCreate failed {nt_status:#010X}");
-        return nt_status;
-    }
-
-    nt_status = unsafe {
-        macros::call_unsafe_wdf_function_binding!(
-            WdfDeviceCreateDeviceInterface,
-            device,
-            &GUID_DEVINTERFACE_HELLO_WORLD,
-            core::ptr::null_mut()
-        )
-    };
-
-    if !nt_success(nt_status) {
-        println!("Error: WdfDeviceCreateDeviceInterface failed {nt_status:#010X}");
-        return nt_status;
-    }
-
-    println!("Exit: evt_device_add");
-
-    0 as NTSTATUS
-}
-
-extern "C" fn evt_driver_unload(driver: WDFDRIVER) {
-    println!("Enter: evt_driver_unload");
-
-    println!("Driver: {:?}", driver);
-
-    println!("Exit: evt_driver_unload");
-}
--- general/hello_world/src/lib.rs
+++ general/hello_world/src/lib.rs
@@ -1,13 +1,69 @@
 #![no_std]
+#![cfg_attr(feature = "nightly", feature(hint_must_use))]
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![allow(clippy::missing_safety_doc)]
+
+mod driver;
 
 #[cfg(not(test))]
 extern crate wdk_panic;
 
-#[cfg(not(test))]
+use core::sync::atomic::AtomicI32;
 use wdk_alloc::WDKAllocator;
-
 #[cfg(not(test))]
 #[global_allocator]
 static GLOBAL_ALLOCATOR: WDKAllocator = WDKAllocator;
 
-mod driver;
+
+use wdk::wdf;
+use wdk_sys::{
+    macros,
+    GUID,
+    NTSTATUS,
+    PVOID,
+    ULONG,
+    WDFOBJECT,
+    WDFREQUEST,
+    WDF_OBJECT_CONTEXT_TYPE_INFO,
+};
+
+
+mod wdf_object_context;
+use wdf_object_context::{wdf_declare_context_type, wdf_declare_context_type_with_name};
+
+const GUID_DEVINTERFACE_HELLO_WORLD: GUID = GUID {
+    Data1: 0x4d36e988,
+    Data2: 0xe315,
+    Data3: 0x11ce,
+    Data4: [0xbf, 0xc1, 0x08, 0x01, 0x2b, 0xe1, 0x03, 0x18],
+};
+
+
+// Declare queue context.
+//
+// ====== CONTEXT SETUP ========//
+
+// The device context performs the same job as
+// a WDM device extension in the driver frameworks
+pub struct DeviceContext {
+    private_device_data: ULONG, // just a placeholder
+}
+wdf_declare_context_type!(DeviceContext);
+
+pub struct QueueContext {
+    buffer: PVOID,
+    length: usize,
+    timer: wdf::Timer,
+    current_request: WDFREQUEST,
+    current_status: NTSTATUS,
+    spin_lock: wdf::SpinLock,
+}
+wdf_declare_context_type_with_name!(QueueContext, queue_get_context);
+
+pub struct RequestContext {
+    cancel_completion_ownership_count: AtomicI32,
+}
+wdf_declare_context_type_with_name!(RequestContext, request_get_context);
